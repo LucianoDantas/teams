2f8a20d07eac2b6ae8e4d847e36178afb9bffcb2
luciano.dantas <luciano.dantas@codeminer42.com>
2020-02-13 08:21:45 -0300

    Init the project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GroupInput from './components/GroupInput';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <GroupInput />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/GroupInput/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.scss';
+
+const GroupInput = () => <form>casinha</form>;
+
+export default GroupInput;
+
+const groupsRankedByScore = [
+  [
+    {
+      name: 'A1',
+      score:
+    }
+  ]
+]
